538c8dcc92517c0e7ee5288b1c4efb2f44852a9a
arinavalkova <31704439+Westochka@users.noreply.github.com>
2020-04-17 09:16:05 +0700

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -9,48 +9,45 @@
 #define BAD_OPEN_TERM "Can't open /dev/tty"
 #define BAD_ARGUMENTS "Don't found name of file"
 #define GET_LINE_FOR_FIVE_SEC "Please, type number of line for 5 seconds\n"
+#define BAD_MEMORY "Can't allocate memory"
+#define BAD_LSEEK "Can't lseek"
 #define MAX_SIZE 1024
 
-int main(int argc, char **argv)
+int openingDiscriptors(int* fin1, int* fin2, char** argv)
 {
-    int fin1, fin2, lineNumber, currentLengthLine = 0, currentLine = 1, *lengths, *carryOvers, temp;
-    lengths = (int *) calloc(MAX_SIZE, sizeof(int));
-    carryOvers = (int *) calloc(MAX_SIZE, sizeof(int));
-    char simbol, *buff;
-    buff = (char *) malloc(MAX_SIZE);
+    if ((*fin1 = open("/dev/tty", O_RDONLY | O_NDELAY)) == -1)
+        return 1;
 
-    if (argc < 2)
-    {
-        perror(BAD_ARGUMENTS);
-        exit(1);
-    }
+    if ((*fin2 = open(argv[1], O_RDONLY)) == -1)
+        return 2;
 
-    if ((fin1 = open("/dev/tty", O_RDONLY | O_NDELAY)) == -1)
-    {
-        perror(BAD_OPEN_TERM);
-        exit(1);
-    }
+    return 0;
+}
 
-    if ((fin2 = open(argv[1], O_RDONLY)) == -1)
-    {
-        perror(BAD_OPEN_FILE);
-        exit(1);
-    }
-
+int readingFile(int* carryOvers, int* lengths, int currentLine, int currentLengthLine, int fin)
+{
+    char simbol;
     carryOvers[currentLine] = 0;
 
-    while (read(fin2, &simbol, 1))
+    while(read(fin, &simbol, 1))
     {
-        if (simbol == '\n')
+        if(simbol == '\n')
         {
+            int ls;
+            if((ls = lseek(fin, 0, 1)) == -1)
+                return -1;
             lengths[currentLine++] = currentLengthLine;
-            carryOvers[currentLine] = lseek(fin2, 0, 1);
+            carryOvers[currentLine] = ls;
             currentLengthLine = 0;
-        } else
+        }
+        else
             currentLengthLine++;
     }
+    return 0;
+}
 
-
+int scanningLines(int fin1, int fin2, char* buff, int* lengths, int* carryOvers, int temp, int lineNumber)
+{
     while (1)
     {
         write(1, GET_LINE_FOR_FIVE_SEC, strlen(GET_LINE_FOR_FIVE_SEC));
@@ -66,7 +63,7 @@ int main(int argc, char **argv)
             free(carryOvers);
             close(fin2);
             close(fin1);
-            exit(0);
+            return 0;
         } else
         {
             lineNumber = atoi(buff);
@@ -75,13 +72,110 @@ int main(int argc, char **argv)
                 write(1, BAD_LINE, strlen(BAD_LINE));
                 continue;
             }
-            lseek(fin2, carryOvers[lineNumber], 0);
+            if(lseek(fin2, carryOvers[lineNumber], 0) == -1)
+                return 1;
 
             char *textLine = (char *) malloc(lengths[lineNumber] + 1);
+
+            if(textLine == NULL)
+                return 2;
+
             if (read(fin2, textLine, lengths[lineNumber] + 1))
                 write(1, textLine, lengths[lineNumber] + 1);
 
             free(textLine);
         }
     }
+}
+
+int main(int argc, char **argv)
+{
+    int fin1, fin2, lineNumber, currentLengthLine = 0, currentLine = 1, *lengths, *carryOvers, temp;
+    lengths = (int *) calloc(MAX_SIZE, sizeof(int));
+
+    if(lengths == NULL)
+    {
+        perror(BAD_MEMORY);
+        exit(1);
+    }
+
+    carryOvers = (int *) calloc(MAX_SIZE, sizeof(int));
+
+    if(carryOvers == NULL)
+    {
+        free(lengths);
+        perror(BAD_MEMORY);
+        exit(1);
+    }
+
+    char simbol, *buff;
+    buff = (char *) malloc(MAX_SIZE);
+
+    if(buff == NULL)
+    {
+        free(lengths);
+        free(carryOvers);
+        perror(BAD_MEMORY);
+        exit(1);
+    }
+
+    if (argc < 2)
+    {
+        free(lengths);
+        free(carryOvers);
+        free(buff);
+        perror(BAD_ARGUMENTS);
+        exit(1);
+    }
+
+    int openD = openingDiscriptors(&fin1, &fin2, argv);
+    if(openD == 1)
+    {
+        free(lengths);
+        free(carryOvers);
+        free(buff);
+        perror(BAD_OPEN_TERM);
+        exit(1);
+    }
+    else if(openD == 2)
+    {
+        free(lengths);
+        free(carryOvers);
+        free(buff);
+        perror(BAD_OPEN_FILE);
+        exit(1);
+    }
+
+    if(readingFile(carryOvers, lengths, currentLine, currentLengthLine, fin2) == -1)
+    {
+        free(lengths);
+        free(carryOvers);
+        perror(BAD_LSEEK);
+        close(fin2);
+        exit(1);
+    }
+
+    int scanL = scanningLines(fin1, fin2, buff, lengths, carryOvers, temp, lineNumber);
+    if(scanL == 1)
+    {
+        free(buff);
+        free(lengths);
+        free(carryOvers);
+        close(fin2);
+        close(fin1);
+        perror(BAD_LSEEK);
+        exit(1);
+    }
+    else if(scanL == 2)
+    {
+        free(buff);
+        free(lengths);
+        free(carryOvers);
+        close(fin2);
+        close(fin1);
+        perror(BAD_MEMORY);
+        exit(1);
+    }
+
+    return 0;
 }
